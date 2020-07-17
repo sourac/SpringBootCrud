309f7fdc05c2311441277426a82a2ef5b81b715a
Atul Sharma <atulsharma21772@gmail.com>
2020-07-17 22:41:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpringBootCrud
+ Crud Ops
---
+++ src/main/java/com/springboot/rest/example/model/Student.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.springboot.rest.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author Atul Sharma
+ *
+ * https://github.com/sourac
+ */
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue
+	private long id;
+	private String name;
+	private String password;
+
+}
