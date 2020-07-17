a42808d5497d46324d22cb56791cdccd1349f4b8
Atul Sharma <atulsharma21772@gmail.com>
2020-07-17 23:38:25 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

--- src/main/java/com/springboot/rest/example/controller/StudentResouceController.java
+++ src/main/java/com/springboot/rest/example/controller/StudentResouceController.java
@@ -4,12 +4,16 @@
 package com.springboot.rest.example.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.rest.example.model.Student;
+import com.springboot.rest.example.respository.StudentNotFoundException;
 import com.springboot.rest.example.respository.StudentRepository;
 
 /**
@@ -27,6 +31,16 @@ public class StudentResouceController {
 	@GetMapping("/students")
 	public List<Student> retrieveAllStudents() {
 		return studentRepository.findAll();
+	}
+	
+	@GetMapping("/student/{id}")
+	public Student retrieveStudent(@PathVariable long id) {
+		// java-8 feature of optional to deal with null pointer exception, loving it..
+		Optional<Student> student=studentRepository.findById(id);
+		if(!student.isPresent()) {
+			throw new StudentNotFoundException("Student with id : " + id + ",  not in the database.");
+		}	
+		return student.get();
 	}
 
 }
